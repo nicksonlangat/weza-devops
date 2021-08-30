01a365004ce38f00813788ba2905390b1e2fec38
nicksonlangat <nicksonlangat95@gmail.com>
2021-08-30 15:49:19 +0300

    added test

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
-
+from .models import Employee
 # Create your tests here.
+
+class EmployeeTest(TestCase):
+    def create_employee(self, first_name='Steve', last_name='Hoober', email='stevehoober@gmail.com', salary=40000):
+        return Employee.objects.create(
+            first_name=first_name,
+            last_name=last_name,
+            email=email,
+            salary=salary
+        )
+    def test_employee_creation(self):
+        x=self.create_employee()
+        self.assertTrue(isinstance(x,Employee))
+        self.assertEqual(x.__str__(), x.first_name)
+
